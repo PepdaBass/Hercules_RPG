a9d383ccb3aaa0c16888eda49afd52729f036991
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2021-12-20 16:02:31 -0500

    Added dictionaries of stats for Hercules and his three opponents.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 import random
+
+def attack():
+
+def RunGame():
+
+hercules_stats = {
+    'Hit Points': 30,
+    'Attack': 11,
+    'Action': ['punch', 'kick', 'bite', 'headbutt']
+}
+
+lion_stats = {
+    'Hit Points': 41,
+    'Attack': 8,
+    'Action': ['bite', 'scratch']
+}
+
+hydra_stats = {
+    'Hit Points': 59,
+    'Attack': 16,
+    'Action': ['bite', 'scratch', 'poison breath']
+}
+
+cerberus_stats = {
+    'Hit Points': 75,
+    'Attack': 13,
+    'Action': ['bite', 'scratch', 'fire breath']
+}
+
